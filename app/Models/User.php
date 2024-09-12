0c77cdfd311d45e2a08463094238124959e9b90c
lyhork <lyhorklim@gmail.com>
2024-09-12 11:59:09 +0700

    middleware

--- app/Models/User.php
+++ app/Models/User.php
@@ -47,6 +47,6 @@ class User extends Authenticatable implements FilamentUser
 
     public function canAccessPanel(Panel $panel): bool
     {
-        return str_ends_with($this->email, 'fsa@admin.com') && $this->hasVerifiedEmail();
+        return str_ends_with($this->email, '@yourdomain.com') && $this->hasVerifiedEmail();
     }
 }
